ca7447974245c02702693ea53a651e82212be1f2
Doug <108644233+DougFaveroAnjos@users.noreply.github.com>
2023-10-05 17:23:52 -0300

    Adicionado a biblioteca AXIOS (comunic a WEB API) e teste em APP.tsx

--- App.tsx
+++ App.tsx
@@ -2,9 +2,28 @@ import {NativeBaseProvider, StatusBar} from 'native-base'
 import {THEMAS} from './src/styles/temas';
 {/**Import das rotas */}
 import Rotas from './src/Rotas';
-
+import { useEffect } from 'react';
+import { API } from './src/servicos/api';
 
 export default function App() {
+
+
+  {/**
+
+  ** Teste de comunicação entre a aplicação e a WEB API
+
+  useEffect(() => {
+    async function testConexao(){
+      const conexao = await API.get('/paciente')
+      console.log(conexao.data)
+    }
+    testConexao()
+  }, [])
+   
+*/}
+
+
+
   return (
     <NativeBaseProvider theme = {THEMAS}>
       <StatusBar backgroundColor = {THEMAS.colors.blue[800]} />
